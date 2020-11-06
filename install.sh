804d63c9846993d48e9e19b08ddc2c87e78f9da2
Peter Gallagher <pete@pjgcreations.co.uk>
2020-11-06 17:55:08 +0000

    Fix DotNet Link again

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ echo -e "\e[1m    Make Link Permanent"
 echo -e "\e[1m----------------------------------------"
 echo -e "\e[0m"
 
-if grep -q 'export DOTNET_ROOT=' home/pi/.bashrc;  then
+if grep -q 'export DOTNET_ROOT=' /home/pi/.bashrc;  then
   echo 'Already added link to .bashrc'
 else
   echo 'Adding Link to .bashrc'
