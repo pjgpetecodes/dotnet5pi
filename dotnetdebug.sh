70b8e75f632b1444e204b62ac7f6223217cd5ef3
Pete Gallagher <pete@pjgcreations.co.uk>
2021-02-06 00:10:57 +0000

    Add comments to debug stub

--- dotnetdebug.sh
+++ dotnetdebug.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash -x
 
+echo -e "\e[1m----------------------------------------"
+echo -e "\e[1m            .NET Debug Stub"
+echo -e "\e[1m----------------------------------------"
+echo ""
+echo -e "\e[1mPete Codes / PJG Creations 2021"
+echo ""
+echo -e "Latest update 06/02/2021"
+echo ""
+echo -e "\e[0mLet me know how you get on by tweeting me at \e[1;5m@pete_codes\e[0m"
+echo ""
+
 export DISPLAY=:0
 
 exec $@
